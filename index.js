481cb8b180bb84127afcaf571566a98a5ba1e36a
Rouaa belhaj Ali <68033931+Rouaa-Belhaj-Ali@users.noreply.github.com>
2024-03-06 12:21:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ import {pipeline} from "@xenova/transformers";
 
 let pipe = await pipeline ('image-classification')
 
-const result =  await pipe('/users/itc/Desktop/test/ll.jpg', {topk: 6}) 
+const result =  await pipe('/.../itr/Desktop/test/ll.jpg', {topk: 6}) 
 
 console.log(result);
